58175660c667b0fff36210b389d46a58fac8b43a
Lê Thanh Hiếu <hieulethanh243@gmail.com>
2024-11-03 23:30:26 +0700

    Update github yml

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -10,13 +10,13 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: `https://${domain}/container/latest/`,
+    publicPath: `/container/latest/`,
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: "marketing@{domain}/marketing/remoteEntry.js",
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
