468a46a586a91e8dec0ad3f030d0a88300c7865e
Lê Thanh Hiếu <hieulethanh243@gmail.com>
2024-11-04 16:41:10 +0700

    Feat: Added marketing component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-// import MarketingApp from "./components/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import {
   StylesProvider,
@@ -17,7 +17,7 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          {/* <MarketingApp /> */}
+          <MarketingApp />
         </div>
       </StylesProvider>
     </BrowserRouter>
