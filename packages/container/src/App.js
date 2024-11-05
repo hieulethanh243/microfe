022690d37c5e7847036db1d36f9c5db2a3e48ebb
Lê Thanh Hiếu <hieulethanh243@gmail.com>
2024-11-05 19:20:45 +0700

    Feat: Auth and  upload auth'

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,23 +1,39 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
-import Header from "./components/Header";
+import React, { lazy, Suspense, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
+import Progress from "./components/Progress";
+
+import Header from "./components/Header";
+
+const AuthAppLazy = lazy(() => import("./components/AuthApp"));
+const MarketingAppLazy = lazy(() => import("./components/MarketingApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
-          <MarketingApp />
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => setIsSignedIn(false)}
+          />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthAppLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/" component={MarketingAppLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
