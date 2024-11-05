3263f7dbf4d06e4ba9e305d548fe82464b27792b
Lê Thanh Hiếu <hieulethanh243@gmail.com>
2024-11-05 13:05:43 +0700

    Feat: Updated the router for them

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,21 @@
-import { mount } from 'marketing/MarketingApp';
-import React, { useRef, useEffect } from 'react';
+import { mount } from "marketing/MarketingApp";
+import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPath }) => {
+        if (history.location.pathname !== nextPath) {
+          history.push(nextPath);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
   });
 
   return <div ref={ref} />;
